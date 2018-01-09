1ec54509ef670e5edf0ba4cea717f6abb8ab0c92
赵博文 <z.bowen66@gmail.com>
2018-01-09 11:49:04 +0800

    Layout布局

--- src/routes/MainLayout.js
+++ src/routes/MainLayout.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import { connect } from 'dva';
 import { routerRedux } from 'dva/router';
-// import styles from './MainLayout.css';
+import { Layout, Menu, Icon } from 'antd';
+import './MainLayout.less';
 
-function MainLayout({ children, login, dispatch }) {
-  if (!login.token) {
-    dispatch(routerRedux.push('/login'))
+const { Header, Footer, Sider, Content } = Layout
+
+class MainLayout extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  componentWillMount() {
+    const { login, dispatch } = this.props
+    if (!login.token) {
+      dispatch(routerRedux.push('/login'))
+    }
   }
-  return (
-    <div>
-      Route Component: MainLayout
-      <button onClick={() => dispatch({ type: 'login/logout' })}>log out</button>
-      <div style={{ background: '#ccc' }}>{children}</div>
-    </div>
-  );
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+  render() {
+    const { children, dispatch } = this.props
+    return (
+      <Layout id="main-layout">
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark">
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>dashboard</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="team" />
+              <span>list</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header>
+            <Icon
+              type={this.state.collapsed ? "menu-unfold" : "menu-fold"}
+              onClick={this.toggle}
+            />
+            <Icon
+              className="fr"
+              type="logout"
+              onClick={() => dispatch({ type: 'login/logout' })}
+            />
+          </Header>
+          <Content>{children}</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 function mapStateToProps({ login }) {
